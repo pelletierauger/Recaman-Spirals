bfd9dfccd8d78a5f88c6dedef7707c594f998de3
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-07-29 19:14:51 -0400

    Create a first version

--- sketch.js
+++ sketch.js
@@ -2,26 +2,37 @@ let looping = true;
 let socket, cnvs, ctx, canvasDOM;
 let fileName = "./frames/sketch";
 let maxFrames = 20;
+let spiral;
 
 function setup() {
     socket = io.connect('http://localhost:8080');
-    cnvs = createCanvas(windowWidth, windowWidth / 16 * 9);
+    cnvs = createCanvas(windowWidth / 16 * 9, windowWidth / 16 * 9);
     ctx = cnvs.drawingContext;
     canvasDOM = document.getElementById('defaultCanvas0');
     frameRate(30);
-    background(0);
-    fill(255, 50);
+    background(210);
+    fill(0, 255);
+    // stroke(0, 255);
     noStroke();
+    // noFill();
+    spiral = new Spiral();
+    translate(width / 2, height / 2);
+    // spiral.show();
+    // for (let i = 0; i < 20; i++) {
+    //     spiral.step();
+    // }
+    console.log(spiral.sequence);
     if (!looping) {
         noLoop();
     }
 }
 
 function draw() {
-    for (let i = 0; i < 500; i++) {
-        let x = random(width);
-        let y = random(height);
-        ellipse(x, y, 5);
+    // background(210);
+    translate(width / 2, height / 2);
+    for (let i = 0; i < 1; i++) {
+        // spiral.drawStep();
+        spiral.showSpiral();
     }
     if (exporting && frameCount < maxFrames) {
         frameExport();
---
+++ spiral.js
@@ -0,0 +1,83 @@
+let Spiral = function() {
+    this.numberLine = [];
+    this.counter = 1;
+    this.sequence = [];
+    this.index = 0;
+    this.drawnIndex = 0;
+    for (let i = 0; i < 3000000; i++) {
+        let x = cos(i) * i * 1;
+        let y = sin(i) * i * 1;
+        this.addNumber(x, y);
+    }
+    for (let i = 0; i < 300000; i++) {
+        this.step();
+    }
+};
+
+Spiral.prototype.step = function() {
+    let next = this.index - this.counter;
+    if (next < 0 || this.numberLine[next].v) {
+        next = this.index + this.counter;
+    }
+    this.numberLine[next].v = true;
+    this.sequence.push(next);
+    this.index = next;
+    this.counter++;
+};
+
+Spiral.prototype.drawStep = function() {
+    // let v1 = this.numberLine[this.index];
+    // let vMid1 = this.numberLine[this.sequence[this.index - 1]] ||  { x: 0, y: 0 };
+    // let v2 = this.numberLine[this.index];
+    // // line(v1.x, v1.y, v2.x, v2.y);
+    // curve(v1.x, v1.y, vMid1.x, vMid1.y, 0, 0, v2.x, v2.y);
+
+    if (this.drawnIndex > 0) {
+        let d = this.drawnIndex;
+        let v0 = this.numberLine[this.sequence[d - 1]];
+        let v1 = this.numberLine[this.sequence[d]];
+        let v2 = this.numberLine[this.sequence[d + 1]];
+        let v3 = this.numberLine[this.sequence[d + 2]];
+        line(v1.x, v1.y, v2.x, v2.y);
+        // curve(v0.x, v0.y, v1.x, v1.y, v2.x, v2.y, v3.x, v3.y);
+    }
+
+    this.drawnIndex++;
+};
+
+Spiral.prototype.addNumber = function(x, y) {
+    this.numberLine.push({
+        x: x,
+        y: y,
+        v: this.numberLine.length ? false : true
+    });
+};
+
+
+Spiral.prototype.showSpiral = function() {
+    // for (let i = 0; i < this.numberLine.length; i++) {
+    //     let n = this.numberLine[i];
+    //     ellipse(n.x, n.y, 1);
+    // }
+    for (let i = 0; i < 250; i++) {
+        let d = this.drawnIndex;
+        let x = cos(this.sequence[d]) * this.sequence[d] * 0.001;
+        let y = sin(this.sequence[d]) * this.sequence[d] * 0.001;
+        let radius = map(this.sequence[d], 0, 50000, 1, 1.5);
+        ellipse(x, y, radius);
+        this.drawnIndex++;
+    }
+
+};
+
+// Spiral.prototype.show = function() {
+//     // for (let i = 0; i < this.numberLine.length; i++) {
+//     //     let n = this.numberLine[i];
+//     //     ellipse(n.x, n.y, 1);
+//     // }
+//     for (let i = 0; i < 30000; i++) {
+//         let x = cos(i) * this.sequence[i] * 0.1;
+//         let y = sin(i) * this.sequence[i] * 0.1;
+//         ellipse(x, y, 1);
+//     }
+// };
